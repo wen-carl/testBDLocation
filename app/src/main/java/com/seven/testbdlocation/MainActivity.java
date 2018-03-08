7a7cca79a1a35d8cd7e3aa374f78074b61b43fb3
wen-carl <wen_carl0701@163.com>
2018-03-08 15:14:02 +0800

    test

--- app/src/main/java/com/seven/testbdlocation/MainActivity.java
+++ app/src/main/java/com/seven/testbdlocation/MainActivity.java
@@ -9,10 +9,14 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     
     private LocationClient mClient;
     private MyLocationListener mListener;
+    private TextView tv;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,58 +32,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         requestPermission();
-        
+
+        tv = findViewById(R.id.msg);
+
         mClient = new LocationClient(getApplicationContext());
         mListener = new MyLocationListener();
         mClient.registerLocationListener(mListener);
-    
-        LocationClientOption option = new LocationClientOption();
-    
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-        //可选，设置定位模式，默认高精度
-        //LocationMode.Hight_Accuracy：高精度；
-        //LocationMode. Battery_Saving：低功耗；
-        //LocationMode. Device_Sensors：仅使用设备；
-    
-        option.setCoorType("bd09ll");
-        //可选，设置返回经纬度坐标类型，默认gcj02
-        //gcj02：国测局坐标；
-        //bd09ll：百度经纬度坐标；
-        //bd09：百度墨卡托坐标；
-        //海外地区定位，无需设置坐标类型，统一返回wgs84类型坐标
-    
-        option.setScanSpan(1000);
-        //可选，设置发起定位请求的间隔，int类型，单位ms
-        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
-        //如果设置非0，需设置1000ms以上才有效
-    
-        option.setOpenGps(true);
-        //可选，设置是否使用gps，默认false
-        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
-    
-        option.setLocationNotify(true);
-        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
-    
-        option.setIgnoreKillProcess(false);
-        //可选，定位SDK内部是一个service，并放到了独立进程。
-        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
-    
-        option.SetIgnoreCacheException(false);
-        //可选，设置是否收集Crash信息，默认收集，即参数为false
-    
-        option.setWifiCacheTimeOut(5*60*1000);
-        //可选，7.2版本新增能力
-        //如果设置了该接口，首次启动定位时，会先判断当前WiFi是否超出有效期，若超出有效期，会先重新扫描WiFi，然后定位
-    
-        option.setEnableSimulateGps(false);
-        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
-    
-        mClient.setLocOption(option);
+        mClient.setLocOption(getDefaultOption());
         //mLocationClient为第二步初始化过的LocationClient对象
         //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
         //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
         
         mClient.start();
+    }
+
+    private LocationClientOption getDefaultOption() {
+        LocationClientOption mOption = new LocationClientOption();
+        mOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        mOption.setCoorType("gcj02");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+        mOption.setScanSpan(3000);//可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
+        mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        mOption.setIsNeedLocationDescribe(true);//可选，设置是否需要地址描述
+        mOption.setNeedDeviceDirect(false);//可选，设置是否需要设备方向结果
+        mOption.setLocationNotify(false);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        mOption.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        mOption.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        mOption.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        mOption.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        mOption.setOpenGps(true);//可选，默认false，设置是否开启Gps定位
+        mOption.setIsNeedAltitude(true);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+        mOption.setWifiCacheTimeOut(5*60*1000);
+
+        return mOption;
+    }
+
+    private LocationClientOption getOption() {
+        LocationClientOption option = new LocationClientOption();
+
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，设置定位模式，默认高精度
+        //LocationMode.Hight_Accuracy：高精度；
+        //LocationMode. Battery_Saving：低功耗；
+        //LocationMode. Device_Sensors：仅使用设备；
+
+        option.setCoorType("gcj02");
+        //可选，设置返回经纬度坐标类型，默认gcj02
+        //gcj02：国测局坐标；
+        //bd09ll：百度经纬度坐标；
+        //bd09：百度墨卡托坐标；
+        //海外地区定位，无需设置坐标类型，统一返回wgs84类型坐标
+
+        option.setScanSpan(2000);
+        //可选，设置发起定位请求的间隔，int类型，单位ms
+        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
+        //如果设置非0，需设置1000ms以上才有效
+
+        option.setOpenGps(true);
+        //可选，设置是否使用gps，默认false
+        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
+
+        option.setLocationNotify(true);
+        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+
+        option.setIgnoreKillProcess(false);
+        //可选，定位SDK内部是一个service，并放到了独立进程。
+        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+
+        option.SetIgnoreCacheException(false);
+        //可选，设置是否收集Crash信息，默认收集，即参数为false
+
+        option.setWifiCacheTimeOut(5*60*1000);
+        //可选，7.2版本新增能力
+        //如果设置了该接口，首次启动定位时，会先判断当前WiFi是否超出有效期，若超出有效期，会先重新扫描WiFi，然后定位
+
+        option.setEnableSimulateGps(false);
+        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+
+        return option;
     }
     
     @TargetApi(23)
@@ -92,8 +122,7 @@ public class MainActivity extends AppCompatActivity {
         allPermission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         allPermission.add(Manifest.permission.CHANGE_WIFI_STATE);
         for (String permission : allPermission) {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), permission) != PackageManager
-                .PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), permission) != PackageManager.PERMISSION_GRANTED) {
                 unGetPermissionList.add(permission);
             }
         }
@@ -108,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
     
     @Override
     protected void onDestroy() {
+        mClient.stop();
         mClient.unRegisterLocationListener(mListener);
         super.onDestroy();
     }
@@ -115,8 +145,91 @@ public class MainActivity extends AppCompatActivity {
     private class MyLocationListener extends BDAbstractLocationListener {
     
         @Override
-        public void onReceiveLocation(BDLocation bdLocation) {
-            Log.i("", "onReceiveLocation: " + bdLocation.getLocType());
+        public void onReceiveLocation(BDLocation location) {
+            StringBuilder sb = new StringBuilder();
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                sb.append("time : ");
+                /**
+                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
+                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
+                 */
+                sb.append(location.getTime());
+                sb.append("\nlocType : ");// 定位类型
+                sb.append(location.getLocType());
+                sb.append("\nlocType description : ");// *****对应的定位类型说明*****
+                sb.append(location.getLocTypeDescription());
+                sb.append("\nlatitude : ");// 纬度
+                sb.append(location.getLatitude());
+                sb.append("\nlontitude : ");// 经度
+                sb.append(location.getLongitude());
+                sb.append("\nradius : ");// 半径
+                sb.append(location.getRadius());
+                sb.append("\nCountryCode : ");// 国家码
+                sb.append(location.getCountryCode());
+                sb.append("\nCountry : ");// 国家名称
+                sb.append(location.getCountry());
+                sb.append("\ncitycode : ");// 城市编码
+                sb.append(location.getCityCode());
+                sb.append("\ncity : ");// 城市
+                sb.append(location.getCity());
+                sb.append("\nDistrict : ");// 区
+                sb.append(location.getDistrict());
+                sb.append("\nStreet : ");// 街道
+                sb.append(location.getStreet());
+                sb.append("\naddr : ");// 地址信息
+                sb.append(location.getAddrStr());
+                sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
+                sb.append(location.getUserIndoorState());
+                sb.append("\nDirection(not all devices have value): ");
+                sb.append(location.getDirection());// 方向
+                sb.append("\nlocationdescribe: ");
+                sb.append(location.getLocationDescribe());// 位置语义化信息
+                sb.append("\nPoi: ");// POI信息
+                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
+                    for (int i = 0; i < location.getPoiList().size(); i++) {
+                        Poi poi = (Poi) location.getPoiList().get(i);
+                        sb.append(poi.getName()).append(";");
+                    }
+                }
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\nspeed : ");
+                    sb.append(location.getSpeed());// 速度 单位：km/h
+                    sb.append("\nsatellite : ");
+                    sb.append(location.getSatelliteNumber());// 卫星数目
+                    sb.append("\nheight : ");
+                    sb.append(location.getAltitude());// 海拔高度 单位：米
+                    sb.append("\ngps status : ");
+                    sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    // 运营商信息
+                    if (location.hasAltitude()) {// *****如果有海拔高度*****
+                        sb.append("\nheight : ");
+                        sb.append(location.getAltitude());// 单位：米
+                    }
+                    sb.append("\noperationers : ");// 运营商信息
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("离线定位成功，离线定位结果也是有效的");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+            } else {
+                sb.append("定位失败 : ").append(null == location ? "null" : location.getLocType());
+            }
+
+            tv.setText(sb.toString());
         }
     }
 }
